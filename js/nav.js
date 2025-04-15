fabb9b85d75beeaa3be3051bfb778d7493f4d3e1
Lesedi Rammutla <69653704+Lesedi-coder07@users.noreply.github.com>
2025-04-15 19:17:30 +0200

    Refactor navigation and update contact information across multiple pages. Replaced close button image with text and improved mobile navigation functionality. Updated course details and fees, and enhanced overall styling for better user experience.

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,21 @@
 const open_nav = document.querySelector('.menu_icon');
 const close_nav = document.querySelector('#close_nav_btn');
 const mobile_nav = document.querySelector('#overlay');
-
+const nav_header = document.querySelector("#header__navbar")
 
 open_nav.addEventListener('click',   () => {
     mobile_nav.style.display = 'flex';
+    nav_header.style.display = 'none'
+    
 })
 
 mobile_nav.addEventListener('click', () => {
     mobile_nav.style.display = 'none';
+     nav_header.style.display = 'flex'
+
 
 } )
+close_nav.addEventListener('click', () => {
+    mobile_nav.style.display = 'none';
+     nav_header.style.display = 'flex'
+})
